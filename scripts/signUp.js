2ec235e2dd542b94cbed8485bad7015ed52efa23
Tempest96 <eldorarempest@gmail.com>
2021-03-25 23:31:37 +0200

    Added regexep for \. before @

--- scripts/signUp.js
+++ scripts/signUp.js
@@ -8,7 +8,7 @@ let password = document.getElementById("password");
 
 //RegExp
 let checkN = /^[a-zA-Z]{3,15}$/;
-let checkE = /^[a-zA-Z0-9]{1,}@[a-zA-Z0-9]{1,5}\.[a-zA-Z]{2,5}$/;
+let checkE = /^([a-zA-Z0-9]{1,}|[a-zA-Z0-9]{1,}\.[a-zA-Z0-9]{1,})@[a-zA-Z0-9]{1,5}\.[a-zA-Z]{2,5}$/;
 let checkP = /^[a-zA-Z0-9]{4,20}$/;
 
 //Змінні для test RegExp
